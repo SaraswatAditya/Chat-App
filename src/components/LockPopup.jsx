9beb6cb0443e3cf43f86a753b6a825ff568fec9f
SaraswatAditya <adityasaraswat92@gmail.com>
2024-08-07 00:23:24 +0530

    Color correction

--- src/components/LockPopup.jsx
+++ src/components/LockPopup.jsx
@@ -30,56 +30,63 @@ const LockPopup = ({ onUnlock }) => {
         alignItems: "center",
         justifyContent: "center",
         zIndex: 1300, // Ensure it is above everything else
-        color: "#080707",
+        color: theme.palette.mode === "light" ? "#080707" : "#F0F4FA",
       }}
     >
       <Box
         sx={{
-          backgroundColor: "white",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#F0F4FA"
+              : theme.palette.background.paper,
           padding: 4,
           borderRadius: 2,
           textAlign: "center",
           zIndex: 1301, // Ensure it is above everything else
         }}
       >
-        <Typography variant="h6" className="p-4">App is Locked</Typography>
+        <Typography variant="h6" className="p-4">
+          App is Locked
+        </Typography>
         <Stack direction="column" spacing={1}>
           <Stack direction="row" spacing={1} justifyContent="center">
             {[1, 2, 3].map((num) => (
-              <Button
-                key={num}
-              >
-                {num}
-              </Button>
+              <Button key={num}>{num}</Button>
             ))}
           </Stack>
           <Stack direction="row" spacing={1} justifyContent="center">
             {[4, 5, 6].map((num) => (
-              <Button
-                key={num}
-              >
-                {num}
-              </Button>
+              <Button key={num}>{num}</Button>
             ))}
           </Stack>
           <Stack direction="row" spacing={1} justifyContent="center">
             {[7, 8, 9].map((num) => (
-              <Button
-                key={num}
-              >
-                {num}
-              </Button>
+              <Button key={num}>{num}</Button>
             ))}
           </Stack>
           <Stack direction="row" spacing={1} justifyContent="center">
-            <Button >*</Button>
-            <Button >0</Button>
+            <Button>*</Button>
+            <Button>0</Button>
             <Button>#</Button>
           </Stack>
         </Stack>
         <Stack direction="row" spacing={1} mt={2} justifyContent="center">
-          <Button onClick={handleUnlock}>Unlock</Button>
-          <Button onClick={handleLock}>Kept Lock</Button>
+          <Button
+            sx={{
+              color: theme.palette.mode === "light" ? "#080707" : "#F0F4FA",
+            }}
+            onClick={handleUnlock}
+          >
+            Unlock
+          </Button>
+          <Button
+            sx={{
+              color: theme.palette.mode === "light" ? "#080707" : "#F0F4FA",
+            }}
+            onClick={handleLock}
+          >
+            Kept Lock
+          </Button>
         </Stack>
       </Box>
     </Box>
